1d91ef3398a9916a87efde76a9b9509e149f6ce1
Dabronzo <ayrtonterceiro@gmail.com>
2022-01-28 15:42:10 +0000

    Implemented first test script

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -4,9 +4,22 @@
 const addition = require("../calc");
 
 describe("Calculator tests", () =>{
+    //possible tests for the addition function
+    //1 - if the two parameters passed are numbers
+    //2 - if one or two parameters are missng
+    //3 - if one or two parameters are strings
+    //4 - if it works with floars
+    //5 - if works with negative numbers
+
     describe("Addition test", () =>{
         test("should return 42 out of 20 + 22", () =>{
             expect(addition(20,22)).toBe(42);
+        })
+        test("should return 57 out of 20 + 37", () =>{
+            expect(addition(20,37)).toBe(57);
+        })
+        test("should return 20.6 of 20 + 0.6", () =>{
+            expect(addition(20,0.6)).toBe(20.6);
         })
     })
     describe("Subtraction test", () =>{
